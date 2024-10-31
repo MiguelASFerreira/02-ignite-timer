99704e574d97742ccf0f082fa075b39ff1ab1e81
MiguelASFerreira <migant1306@gmail.com>
2024-10-31 20:01:41 -0300

    Listagem do histórico

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -75,7 +75,7 @@ export function CyclesContextProvider({
     setCycles((state) =>
       state.map((cycle) => {
         if (cycle.id === activeCycleId) {
-          return { ...cycle, finishDate: new Date() }
+          return { ...cycle, interruptedDate: new Date() }
         } else {
           return cycle
         }
